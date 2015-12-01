cfe63f4ba94d340a466cc880e33c1b6c6ced36d3
Shannon <smwhelchel@gmail.com>
2015-11-30 16:15:42 -0800

    show/hide join a carpool and testimonials

--- js/app.js
+++ js/app.js
@@ -1,2 +1,7 @@
-var rideSearch = document.getElementById('search-ride')
-rideSearch.setAttribute('class', 'show');
+var rideSearch = document.getElementById('find-ride')
+var myButton = document.getElementById('search-ride')
+myButton.addEventListener('click', function() { rideSearch.className = 'show' })
+
+var getTestimonials = document.getElementById('testimonials-container')
+var myButton = document.getElementById('get-testimonials')
+myButton.addEventListener('click', function() { getTestimonials.className = 'show' })
